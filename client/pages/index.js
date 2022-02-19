df1d201f7a0b4d2f475202ff202b41b5666234f8
amirrezafahimi <amirrezafahimi98@gmail.com>
2022-02-19 22:00:39 +0330

    developing front-end

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const LandingPage = ({currentUser}) => {
-
+    console.log(currentUser);
+    return (
+        <h1>Landing Page</h1>
+    );
 };
 
 LandingPage.getInitialProps = async () => {
