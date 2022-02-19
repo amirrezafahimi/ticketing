0f9917c970b649be6954364ae535efe401d8b25c
amirrezafahimi <amirrezafahimi98@gmail.com>
2022-02-19 21:58:42 +0330

    developing front-end

---
+++ client/hooks/use-request.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import React, {useState} from "react";
+
+export default ({url, method, body, onSucess}) => {
+    const [errors, setErrors] = useState(null);
+
+    const doRequest = async () => {
+        try {
+            setErrors(null);
+            const response = await axios[method](url, body);
+
+            if (onSucess) {
+                onSucess(response.data);
+            }
+
+            return response.data;
+        } catch (e) {
+            setErrors(
+                <div className="alert alert-danger">
+                    <h4>Ooops...</h4>
+                    <ul className="my-0">
+                        {errors.map(err => <li key={err.message}>{err.message}</li>)}
+                    </ul>
+                </div>
+            );
+        }
+    };
+
+    return {doRequest, errors};
+};
---
+++ client/pages/index.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const LandingPage = ({currentUser}) => {
+
+};
+
+LandingPage.getInitialProps = async () => {
+    if (typeof window === "undefined") {
+        const {data} = await axios.get(
+            "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser", {
+                headers: {
+                    Host: "ticketing.dev"
+                }
+            }
+        );
+        return data;
+    } else {
+        const {data} = await axios.get("/api/users/currentuser");
+        return data;
+    }
+};
+
+export default LandingPage;
