05256095d4f91c94eb6359f893c070db3bd395d1
amirrezafahimi <amirrezafahimi98@gmail.com>
2022-03-01 13:21:03 +0330

    decline for tickets that are already reserved

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response} from "express";
 import {body} from "express-validator";
-import {requireAuth, validateRequest, NotFoundError, NotAuthorizedError} from "@k8s-course/common";
+import {requireAuth, validateRequest, NotFoundError, NotAuthorizedError, BadRequestError} from "@k8s-course/common";
 import {Ticket} from "../models/ticket";
 import {TicketUpdatedPublisher} from "../events/publishers/ticket-updated-publisher";
 import {natsWrapper} from "../nats-wrapper";
@@ -20,6 +20,10 @@ router.put("/api/tickets/:id", requireAuth, [
 
     if (!ticket) {
         throw new NotFoundError();
+    }
+
+    if (ticket.orderId) {
+        throw new BadRequestError("Cannot edit a reserved ticket");
     }
 
     if (ticket.userId !== req.currentUser!.id) {
