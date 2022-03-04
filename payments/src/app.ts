bc4f483159c84cb3daadf1fac2d57f3332e69a08
amirrezafahimi <amirrezafahimi98@gmail.com>
2022-03-04 12:02:12 +0330

    initial setup for payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,29 @@
+import express, {Request, Response} from 'express';
+import 'express-async-errors';
+import {json} from 'body-parser';
+import cookieSession from "cookie-session";
+import {errorHandler, NotFoundError, currentUser} from "@k8s-course/common";
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== "test"
+    })
+);
+app.use(currentUser);
+
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).send();
+});
+
+app.all("*", async (req: Request, res: Response) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export {app};
